f5bf2e6373cf198b66dd84bf84c0a6ff6552e956
isocitrate <emailjustinchen@gmail.com>
2026-05-29 23:41:53 -0500

    Remove unused Midway reference files

--- midway_code/a1-isocitrate-main/q2b.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.loadtxt("q2a_out.csv", delimiter = ",")
-
-plt.scatter(data[:, 0], data[:, 1])
-plt.xlabel("Rho")
-plt.ylabel("Average period to negative health")
-plt.savefig("q2b_plot.png", dpi=300)
